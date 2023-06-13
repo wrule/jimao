87034b94ddd7a8c86c25da0ef502f86e89cf3595
jimao <jimao@jimaodeMacBook-Pro.local>
2023-06-13 17:43:51 +0800

    验证签名

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,23 @@
 import type { NextPage } from 'next';
 import style from './index.module.scss';
+import { Button } from 'antd/lib';
+import { useSignMessage } from 'wagmi';
+import { ethers } from 'ethers';
 
 const Home: NextPage = () => {
-  return <div>你好，世界</div>;
+  const { data, isError, isLoading, isSuccess, signMessageAsync } = useSignMessage({
+    message: 'jimao'
+  });
+  return <div>
+    <Button type="primary" loading={isLoading} onClick={async () => {
+      const data = await signMessageAsync();
+      console.log(data);
+      const address = ethers.verifyMessage('jimao', data);
+      console.log(address);
+    }}>签名消息</Button>
+    {isSuccess && <div>Signature: {data}</div>}
+    {isError && <div>Error signing message</div>}
+  </div>;
 };
 
 export default Home;

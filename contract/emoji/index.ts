bbaedecb0543829c6616cbfdfffb20f54a8bd02d
jimao <jimao@jimaodeMacBook-Pro.local>
2023-06-15 17:03:41 +0800

    合约读取

--- contract/emoji/index.ts
+++ contract/emoji/index.ts
@@ -1,7 +1,7 @@
 import abi from './abi.json';
 
 export
-const Emoji = {
-  address: '0x70C38C4279ca77020CB0551465d0783011D869ca',
+const EmojiInfo = {
+  address: '0x70C38C4279ca77020CB0551465d0783011D869ca' as `0x${string}`,
   abi: abi.abi,
 };
--- pages/emoji/index.tsx
+++ pages/emoji/index.tsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useContractRead } from 'wagmi';
 import style from './index.module.scss';
+import { EmojiInfo } from '../../contract/emoji';
 
 export default
 function Emoji() {
-  return <div className={style.page}>Emoji😄</div>;
+  const { data, isError, isLoading } = useContractRead({
+    address: EmojiInfo.address,
+    abi: EmojiInfo.abi,
+    functionName: 'name',
+  });
+  const [hydrated, setHydrated] = React.useState(false);
+  useEffect(() => {
+    setHydrated(true);
+  }, []);
+  if (!hydrated) return <></>;
+  return <div className={style.page}>{data as any}</div>;
 }

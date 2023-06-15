06a2e379339c05d1a7e31fe71c4dda88b1b64c44
jimao <jimao@jimaodeMacBook-Pro.local>
2023-06-15 14:43:36 +0800

    switch的逻辑

--- pages/chains/scroll/scrollTestnet.ts
+++ pages/chains/scroll/scrollTestnet.ts
@@ -1,4 +1,5 @@
 import { scrollTestnet } from 'wagmi/chains';
+import { Chain } from '@rainbow-me/rainbowkit';
 import scrollLogo from './scroll.ico';
 
 const chain: any = { ...scrollTestnet };
@@ -6,4 +7,4 @@ chain.hasIcon = true;
 chain.iconBackground = 'rgb(235, 113, 6)';
 chain.iconUrl = scrollLogo.src;
 
-export default chain;
+export default chain as Chain;
--- pages/switch/index.tsx
+++ pages/switch/index.tsx
@@ -1,14 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import { useNetwork, useSwitchNetwork } from 'wagmi';
+import { Button } from 'antd/lib';
+import scrollTestnet from '../chains/scroll/scrollTestnet';
 
 export default
 function Switch() {
   const network = useNetwork();
-  const switchNetwork = useSwitchNetwork();
+  const switchNetwork = useSwitchNetwork({ chainId: scrollTestnet.id });
   const [hydrated, setHydrated] = React.useState(false);
   useEffect(() => {
     setHydrated(true);
   }, []);
   if (!hydrated) return <></>;
-  return <div></div>;
+  return <div>
+    <Button
+      type="primary"
+      loading={switchNetwork.isLoading && switchNetwork.pendingChainId === scrollTestnet.id}
+      disabled={network.chain?.id === scrollTestnet.id}
+      onClick={() => switchNetwork.switchNetwork?.()}>
+      切换到Scroll Testnet
+    </Button>
+    {/* <pre>{JSON.stringify({ ...switchNetwork, chains: undefined, data: undefined }, null, 2)}</pre> */}
+  </div>;
 }
